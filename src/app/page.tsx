6081b020b3d0b52630b2ffcbdf9ce407d79407f1
ajay <ap221882@gmail.com>
2024-08-30 01:28:19 +0530

    alignment fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import { ModeToggle } from '@/components/customComponents/toggleTheme';
 export default function Home() {
   return (
     <main >
-      <nav>
-        <ModeToggle/>
+      <nav className='flex flex-row justify-between p-8'>
         <CustomNavigationMenu/>
+        <ModeToggle/>
       </nav>
       <div className="py-36 px-40">
       <div>
