2f4b51b234617ac87ae30d7f453b6c712ec12728
ajay <ap221882@gmail.com>
2024-08-30 01:37:46 +0530

    added separator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { CustomNavigationMenu } from '@/components/customComponents/navBar';
 import { ModeToggle } from '@/components/customComponents/toggleTheme';
+import { Separator } from '@/components/ui/separator';
 
 export default function Home() {
   return (
     <main >
-      <nav className='flex flex-row justify-between p-8'>
+      <nav className='flex flex-row justify-between p-8 pb-4'>
         <CustomNavigationMenu/>
         <ModeToggle/>
       </nav>
+      <Separator className="mx-6" />
       <div className="py-36 px-40">
       <div>
         <h1 className="text-7xl font-extrabold font-sans">Welcome!</h1>
