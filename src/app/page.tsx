a0aa4f95ad54509fb0d0654b22778f3d600acfa1
ajay <ap221882@gmail.com>
2024-08-30 00:59:24 +0530

    added shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 export default function Home() {
   return (
-    <main className="py-36 px-40">
+    <main >
+      <nav>
+        {/* <h4 className="font-light">Ajay Pathak</h4> */}
+      </nav>
+      <div className="py-36 px-40">
       <div>
         <h1 className="text-7xl font-extrabold font-sans">Welcome!</h1>
       </div>
       <div>
-        {/* <div>Line</div> */}
         <div>
           <h3 className="py-4 text-2xl">This is Ajay Pathak.</h3>
         </div>
+      </div>
       </div>
     </main>
   );
